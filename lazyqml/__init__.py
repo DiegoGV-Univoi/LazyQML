3b0d7ea968890e9aad97f1cb1296c7c5211911eb
DiegoGV-Uniovi <garciavdiego@uniovi.es>
2024-05-06 12:58:25 +0200

    Bump version: 0.1.1 → 0.1.2

--- lazyqml/__init__.py
+++ lazyqml/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Diego García Vega, Fernando Álvaro Plou Llorente, Alejandro Leal Castaño"""
 __email__ = "garciavdiego@uniovi.es, ploufernando@uniovi.es, lealcalejandro@uniovi.es"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
