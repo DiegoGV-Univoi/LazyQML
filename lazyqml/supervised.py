6fa3f67793d7abcb2457d05763d47dbbb8056b70
DiegoGV-Uniovi <garciavdiego@uniovi.es>
2024-05-10 11:40:27 +0200

    FIX IMPORT AGAIN

--- lazyqml/supervised.py
+++ lazyqml/supervised.py
@@ -6,7 +6,7 @@
  Import modules
 """
 
-from common import *
+from .common import *
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -689,17 +689,3 @@ class QuantumClassifier():
         
         print(scores.to_markdown())
         return scores, predictions_df if self.predictions is True else scores
-
-from sklearn.datasets import load_breast_cancer, load_iris
-from sklearn.model_selection import train_test_split
-data = load_breast_cancer()
-X = data.data
-y = data.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.3,random_state =123)  
-#print(issubclass(optax.adam.__annotations__["return"],optax._src.base.GradientTransformation) )
-
-
-q = QuantumClassifier(nqubits=2,classifiers=["qnn"],optimizer=optax.adabelief,learningRate=0.1,verbose=True)
-
-scores, predicitons = q.fit(X_train, X_test, y_train, y_test)
