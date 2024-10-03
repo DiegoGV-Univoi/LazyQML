bd663c1f90011a62830ef2692b941eb805d7ebfb
diego <diego@DESKTOP-RNTL2K4.>
2024-10-03 15:46:25 +0200

    Implemented QSVM

--- lazyqml/Factories/Circuits/Qkernel.py
+++ lazyqml/Factories/Circuits/Qkernel.py
@@ -7,11 +7,12 @@ class QkernelCircuit(Circuit):
 
         self.nqubits = nqubits
         self.embedding = embedding
+        self.CircuitFactory = CircuitFactory()
 
     def getCircuit(self):
 
         def kernel_circ(a, b):
-            EmbeddingFactory.GetEmbeddingCircuit(self.embedding)(a, wires = range(self.nqubits))
-            qml.adjoint(EmbeddingFactory.GetEmbeddingCircuit(self.embedding))(b, wires = range(self.nqubits)))
+            self.CircuitFactory.GetEmbeddingCircuit(self.embedding)(a, wires = range(self.nqubits))
+            qml.adjoint(self.CircuitFactory.GetEmbeddingCircuit(self.embedding))(b, wires = range(self.nqubits)))
 
         return kernel_circ
--- lazyqml/Factories/Models/QSVM.py
+++ lazyqml/Factories/Models/QSVM.py
@@ -1,9 +1,57 @@
 from Interfaces.iModel import Model
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
+import pennylane as qml
+from time import time
+from Interfaces.iModel import Model
+from Circuits.fCircuits import CircuitFactory
 
-class QNN(Model):
+class QSVM(Model):
+    def __init__(self, nqubits, embedding):
+        super().__init__()
+        self.nqubits = nqubits
+        self.embedding = embedding
+        self.device = qml.device('lightning.gpu', wires=nqubits)
+        self.CircuitFactory = CircuitFactory(nqubits)
+        self.kernel = self._build_kernel()
 
-    def fit(X, y, self):
-        return super().fit(y, self)
+    def _build_kernel(self):
+        """Build the quantum kernel using a given embedding and ansatz."""
+        # Get the embedding circuit from the circuit factory
+        embedding_circuit = self.CircuitFactory.GetEmbeddingCircuit(self.embedding)
+        
+        # Define the kernel circuit with adjoint embedding for the quantum kernel
+        @qml.qnode(self.device, diff_method=None)
+        def kernel(x1, x2):
+            embedding_circuit(x1, wires=range(self.nqubits))
+            qml.adjoint(embedding_circuit)(x2, wires=range(self.nqubits))
+            return [qml.expval(qml.PauliZ(i)) for i in range(self.nqubits)]
+        
+        return kernel
     
-    def predict(X, self):
-        return super().predict(self)
+    # Not used at the moment, We might be interested in computing our own kernel.
+    def _quantum_kernel(self, X1, X2):
+        """Calculate the quantum kernel matrix for SVM."""
+        num_samples_1 = len(X1)
+        num_samples_2 = len(X2)
+        kernel_matrix = np.zeros((num_samples_1, num_samples_2))
+
+        for i in range(num_samples_1):
+            for j in range(num_samples_2):
+                kernel_matrix[i, j] = self.kernel(X1[i], X2[j]).sum()
+
+        return kernel_matrix
+
+    def fit(self, X, y):
+        # Train the classical SVM with the quantum kernel
+        print("Training the SVM...")
+        self.svm = SVC(kernel=self.kernel)
+        self.svm.fit(X, y)
+        print("SVM training complete.")
+
+    def predict(self, X):
+        y_pred = self.svm.predict(X)
+        return y_pred
