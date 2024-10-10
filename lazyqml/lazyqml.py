8143531d48fc4870b77352fdaa0d18fcb5c66434
diego <diego@DESKTOP-RNTL2K4.>
2024-10-10 20:55:30 +0200

    Initial Jupyter template

--- lazyqml/lazyqml.py
+++ lazyqml/lazyqml.py
@@ -212,11 +212,11 @@ classifier = QuantumClassifier(nqubits=2,classifiers={Model.QNN},embeddings={Emb
 from sklearn.datasets import load_breast_cancer,load_iris
 from sklearn.model_selection import train_test_split
 # Load data
-data = load_breast_cancer()
+data = load_iris()
 X = data.data
 y = data.target
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.95,random_state =1234)  
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.9,random_state =1234)  
 
 classifier.fit(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test)
