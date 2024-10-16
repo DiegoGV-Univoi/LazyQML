ded640e68bd2e44d565ed0c11e62754349cd1e43
garciavdiego <garciavdiego@gmail.com>
2024-10-16 14:55:15 +0200

    Minor fix

--- lazyqml/Factories/Preprocessing/Pca.py
+++ lazyqml/Factories/Preprocessing/Pca.py
@@ -3,20 +3,31 @@ from sklearn.decomposition import PCA
 from sklearn.exceptions import NotFittedError
 
 class Pca(Preprocessing):
-    def __init__(self, nqubits):
+    def __init__(self, nqubits, ncomponents):
         self.nqubits = nqubits
-        self.ncomponents = nqubits
+        self.ncomponents = ncomponents
+        self.fitted = False
         self.preprocessing = PCA(n_components=self.ncomponents)
 
     def fit(self, X, y):
-        return self.preprocessing.fit(X, y) if self.ncomponents <= X.shape[1] else X
+        if self.ncomponents <= X.shape[1]:
+            self.fitted = True
+            fitted_X = self.preprocessing.fit(X, y)
+        else:
+            fitted_X = X
+        return fitted_X
 
     def fit_transform(self, X, y):
-        return self.preprocessing.fit_transform(X, y) if self.ncomponents <= X.shape[1] else X
+        if self.ncomponents <= X.shape[1]:
+            self.fitted = True
+            fitted_X = self.preprocessing.fit_transform(X, y)
+        else:
+            fitted_X = X
+        return fitted_X
 
     def transform(self, X):
-        try:
+        if self.fitted:
             fitted_X = self.preprocessing.transform(X)
-        except:
+        else:
             fitted_X = X
         return fitted_X
--- lazyqml/Factories/Preprocessing/fPreprocessing.py
+++ lazyqml/Factories/Preprocessing/fPreprocessing.py
@@ -3,11 +3,11 @@ from Global.globalEnums import *
 
 # Importing Preprocessings
 from Factories.Preprocessing.Pca import *
+# from Factories.Preprocessing.PcaAmp import *
+# from Factories.Preprocessing.PcaTree import *
+# from Factories.Preprocessing.PcaTreeAmp import *
 from Factories.Preprocessing.Sanitizer import *
 
-from Factories.Preprocessing._PcaAmp import *
-from Factories.Preprocessing._PcaTreeAmp import *
-from Factories.Preprocessing._PcaTree import *
 
 class PreprocessingFactory:
     def __init__(self, nqubits) -> None:
@@ -18,10 +18,23 @@ class PreprocessingFactory:
 
     def GetPreprocessing(self, embedding, ansatz):
         if embedding == Embedding.AMP and ansatz == Ansatzs.TREE_TENSOR:
-            return PcaTreeAmp(self.nqubits)
+            return Pca(self.nqubits, 2**(2**(self.nqubits.bit_length()-1)))
         elif embedding == Embedding.AMP:
-            return PcaAmp(self.nqubits)
+            return Pca(self.nqubits, 2**self.nqubits)
         elif ansatz == Ansatzs.TREE_TENSOR:
-            return PcaTree(self.nqubits)
+            return Pca(self.nqubits, 2**(self.nqubits.bit_length()-1))
         else:
-            return Pca(self.nqubits)
+            return Pca(self.nqubits, self.nqubits)
+
+        """
+        Deprecated
+
+        # if prep == Preprocessing.PCA:
+        #     return Pca(self.nqubits)
+        # elif prep == Preprocessing.PCA_AMP:
+        #     return PcaAmp(self.nqubits)
+        # elif prep == Preprocessing.PCA_TREE:
+        #     return PcaTree(self.nqubits)
+        # elif prep == Preprocessing.PCA_TREE_AMP:
+        #     return PcaTreeAmp(self.nqubits)
+        """
